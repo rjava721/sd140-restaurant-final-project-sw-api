d8359177e54cc97e1006a93a70f90ce9b5d79bde
Jawad <rjava721@gmail.com>
2020-01-10 21:38:49 -0600

    test

--- js/sw.js
+++ js/sw.js
@@ -55,7 +55,11 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      if (response !== null) {
+        return response;
+      }
+      
+      fetch(event.request);
     })
   );
 })
